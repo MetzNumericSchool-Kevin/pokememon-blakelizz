a2877352cc3d2031a258799d3c80ed0e3bc3ba2a
blakelizz <oceane.mngs@yahoo.com>
2025-03-25 21:17:56 +0100

    Attrapage de pokémon réussi !

--- main.js
+++ main.js
@@ -23,64 +23,69 @@ function tabPokemon(listePokemon) {
   console.log(listePokemon);
 
   //divise le tableau et le re mélange
-  const halfListePokemon = listePokemon.slice(0, 6);
+  const halfListePokemon = listePokemon.slice(0, 6); //A MODIFIER AVEC 6
   halfListePokemon.sort(() => Math.random() - 0.5);
   console.log(halfListePokemon);
 
   //Multiplie le tableau divisé
   const duplicateListePokemon = [...halfListePokemon, ...halfListePokemon];
   console.log(duplicateListePokemon);
-  
+
   return duplicateListePokemon;
 }
 
 const duplicateListePokemon = tabPokemon(listePokemon);
 
-
-//---Test avec const ---
-// const pairOfPokemonIds = [
-//   {
-//     "name": "charmander",
-//     "sprite": "https://img.pokemondb.net/sprites/scarlet-violet/normal/charmander.png"
-//   },
-//   {
-//     "name": "squirtle",
-//     "sprite": "https://img.pokemondb.net/sprites/scarlet-violet/normal/squirtle.png"
-//   }
-// ]
-// console.log(pairOfPokemonIds);
-
-
 const grille = document.querySelector("#grille_de_jeu");
 const boxes = document.querySelectorAll(".box");
 const bush = document.querySelectorAll(".bush");
-
 const pokemonCaptures = document.querySelector(".liste_pokemons_captures");
 
+let pokemonCLick = [];
 
 if (bush) {
 
   bush.forEach((bush, index) => {
     bush.addEventListener("click", () => {
-      
+      const pokemonName = duplicateListePokemon[index].name;
       const pokemonSprite = document.createElement("img");
-      // pokemonSprite.src = pairOfPokemonIds[index].sprite; 
       pokemonSprite.src = duplicateListePokemon[index].sprite;
       bush.parentElement.appendChild(pokemonSprite); //ajout du pokemon
 
-      pokemonSprite.style.width = "100%";
-      pokemonSprite.style.height = "100%";
-
+      pokemonSprite.classList.add('pokemon');
 
       bush.style.display = "none"; //cache le bush
-      pokemonSprite.style.display = "block";//affiche le pokemon
 
-      if (pokemonSprite) {
-        const pokemonClick = duplicateListePokemon[index].name;
-        console.log(pokemonClick);
+      pokemonCLick.push({ name: pokemonName, index: index, sprite: pokemonSprite });
+      console.log(pokemonCLick);
+
+      if (pokemonCLick.length == 2) {
+        const [pokemonA, pokemonB] = pokemonCLick;
+
+        if (pokemonA.name == pokemonB.name) {
+
+          setTimeout(() => {
+              document.querySelectorAll('img.pokemon').forEach(img => {
+              const pokeballSprite = document.createElement("img");
+              pokeballSprite.classList.add('pokeball');
+              pokeballSprite.src = "assets/pokeball.png";
+              img.parentElement.appendChild(pokeballSprite);
+            });
+          }, 500);
+
+        } else { 
+          setTimeout(() => {
+            //ré affichage du bush pokemon A et B
+            document.querySelectorAll('.bush')[pokemonA.index].style.display = "block"; 
+            document.querySelectorAll('.bush')[pokemonB.index].style.display = "block"; 
+            // Supp le pokemonSprite
+            pokemonA.sprite.remove();
+            pokemonB.sprite.remove();
+          }, 1000);
+        }
+        //ré initialisation du tableau
+        pokemonCLick = [];
       }
-
-
     });
   });
 }
